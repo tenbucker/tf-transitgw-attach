37669a89fdd7a72f3dd4cb1c654ddca63be05f5a
tenbucker <tenbucker@hotmail.co.uk>
2022-09-28 07:52:40 +0100

    TGW VAR

--- variables.tf
+++ variables.tf
@@ -26,19 +26,24 @@ variable "vpc_cidr" {
 variable "vpc_private_subnets" {
   description = "Private subnets for VPC"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
+  default     = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24", "10.1.4.0/24"]
 }
 
 variable "vpc_public_subnets" {
   description = "Public subnets for VPC"
   type        = list(string)
-  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24", "10.0.104.0/24"]
+  default     = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24", "10.1.104.0/24"]
 }
 
 variable "vpc_enable_nat_gateway" {
   description = "Enable NAT gateway for VPC"
   type        = bool
   default     = true
+}
+
+variable "transit_gateway_id" {
+  description = "TGW ID"
+  default     = "tgw-03f97b313b1e55825"
 }
 
 variable "vpc_tags" {
