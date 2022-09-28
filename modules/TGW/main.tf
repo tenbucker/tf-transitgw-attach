25a9683b0e65d12286cd0f509ffee99b93b22be6
tenbucker <tenbucker@hotmail.co.uk>
2022-09-28 08:51:59 +0100

    TGW VAR

--- modules/TGW/main.tf
+++ modules/TGW/main.tf
@@ -3,7 +3,7 @@ resource "aws_ec2_transit_gateway_route_table_association" "example" {
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.example.id
 }
 resource "aws_ec2_transit_gateway_vpc_attachment" "example" {
-  subnet_ids         = [aws_subnet.example.id]
+  subnet_ids         = var.subnet_ids
   transit_gateway_id = var.transit_gateway_id
   vpc_id             = var.vpc_id
 }
