c131876a4a2b37008d5b5cf8291ac74b480e6c90
tenbucker <tenbucker@hotmail.co.uk>
2022-09-28 11:05:37 +0100

    TGW output

--- outputs.tf
+++ outputs.tf
@@ -12,12 +12,12 @@ output "subnet_id" {
 
 output "aws_ec2_transit_gateway_vpc_attachment" {
   description = "tgw-attach-id"
-  value       = module.aws_ec2_transit_gateway_vpc_attachment.example.id
+  value       = module.TGW.aws_ec2_transit_gateway_vpc_attachment.example.id
   
 }
 
 output "aws_ec2_transit_gateway_route_table" {
   description = "tgw-attach-id"
-  value       = module.aws_ec2_transit_gateway_route_table.example.id
+  value       = module.TGW.aws_ec2_transit_gateway_route_table.example.id
   
 }
