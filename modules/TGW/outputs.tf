81db9c3fe6f100db31f5f7945b7d1ef5d2d6108c
tenbucker <tenbucker@hotmail.co.uk>
2022-09-29 04:45:28 +0100

    TGW output

--- modules/TGW/outputs.tf
+++ modules/TGW/outputs.tf
@@ -1,11 +1,11 @@
-output "transit_gateway_vpc_attachment" {
-  description = "tgw-attach-id"
-  value       = aws_ec2_transit_gateway_vpc_attachment.example.id
-  
-}
+#soutput "transit_gateway_vpc_attachment" {
+#  description = "tgw-attach-id"
+#  value       = aws_ec2_transit_gateway_vpc_attachment.example.id
+#  
+#}
 
-output "transit_gateway_route_table" {
-  description = "tgw-attach-id"
-  value       = aws_ec2_transit_gateway_route_table.example.id
-  
-}
+#output "transit_gateway_route_table" {
+#  description = "tgw-attach-id"
+#  value       = aws_ec2_transit_gateway_route_table.example.id
+#  
+#}
