62d9ac5fbb544ea50fdfe5d0702ccb014c2f138f
tenbucker <tenbucker@hotmail.co.uk>
2022-09-29 06:19:27 +0100

    TGW output

--- modules/TGW/main.tf
+++ modules/TGW/main.tf
@@ -7,7 +7,7 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "example" {
   subnet_ids         = var.subnet_ids
   transit_gateway_id = var.transit_gateway_id
   vpc_id             = var.vpc_id
-  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_association = true
 }
 
 #resource "aws_ec2_transit_gateway_route_table" "example" {
