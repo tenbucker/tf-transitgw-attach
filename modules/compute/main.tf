58c24619cbc123f19d457167ec8091f9151e86b2
tenbucker <tenbucker@hotmail.co.uk>
2022-09-29 06:04:47 +0100

    TGW output

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -38,9 +38,9 @@ resource "aws_security_group" "sg_8080" {
   vpc_id      = var.vpc_id
   name = "${var.prefix}-sg"
   ingress {
-    from_port   = "8080"
-    to_port     = "8080"
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
